04cf4439b05ad3a0dc88d0147215ea33ffab1ddd
Edilson Cichon <edilsoncichon@hotmail.com>
2018-09-03 17:59:22 -0300

    Initial files for register module

---
+++ README.md
@@ -0,0 +1,12 @@
+# Customers Extra Fields for Magento 2
+
+O objetivo deste módulo é extender o módulo 'magento-module-customers', 
+habilitando um conjunto de informações úteis para lojas brasileiras.
+
+## Examples
+ * CPF
+ * RG
+ * dentre outras informações
+ 
+## Como instalar
+// TODO
---
+++ registration.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Customers Extra Fields
+ *
+ * Extra Fields Brazilian for Customers module.
+ *
+ * @package Cichon\CustomersExtraFields
+ * @author Edilson Cichon <edilsoncichon@hotmail.com>
+ * @copyright Copyright (c) 2018 Edilson Cichon (http://edilson.xyz)
+ * @license https://opensource.org/licenses/OSL-3.0.php Open Software License 3.0
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Cichon_CustomersExtraFields',
+    __DIR__
+);
