0e9208b81b055409c3ca113672c0a66c620e2081
Edilson Cichon <edilsoncichon@hotmail.com>
2018-09-03 18:38:53 -0300

    Name correction of module.

--- registration.php
+++ registration.php
@@ -12,6 +12,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Cichon_CustomersExtraFields',
+    'Cichon_CustomerExtraFields',
     __DIR__
 );
